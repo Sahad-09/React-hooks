964c2d7763e6618253164d63c5407adfc0621adb
Sahad <msahad124@gmail.com>
2023-05-25 00:34:32 +0530

    chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import UseState from './Routes/UseState';
+import UseState from './Routes/jsx modules/UseState';
+import UseEffect from './Routes/jsx modules/UseEffect';
 import Home from './Home';
 import { BrowserRouter, Route, Routes   } from 'react-router-dom'
 
@@ -9,8 +10,9 @@ function App() {
 
     <BrowserRouter>
     <Routes>
-      <Route path='/useState' element={<UseState />}/>
       <Route path='/' element={<Home />}/>
+      <Route path='/useState' element={<UseState />}/>
+      <Route path='/useEffect' element={<UseEffect />}/>
 
       
     </Routes>
---
+++ src/Routes/jsx modules/UseState.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from '../css modules/useState.module.css'
+
+
+const UseState = () => {
+  return (
+    <>
+      <div className="banner">
+      <h1 className={styles.heading}>useState</h1>
+      <p>Lets now observe the few real use cases and i'll guide you through how to think in react</p>
+      <h3>1.State Management</h3>
+      <p>Lets say we want to change the state of something with based on the action performed byt the user.</p>
+      </div>
+    </>
+  )
+}
+
+export default UseState
+
+
